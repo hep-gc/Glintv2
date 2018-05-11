84bf88d369cc7fe256179cde48c06c73d006144f
Colson Driemel <colsond@uvic.ca>
2018-05-11 10:48:18 -0700

    remove engine creation from get_db_base_and_session
    This will hopefully help reduce the number of connections.

--- glintwebui/db_util.py
+++ glintwebui/db_util.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
+Base = automap_base()
+Engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + \
+                       "@" + config.db_host+ ":" + str(config.db_port) + "/" + config.db_name)
 
 def get_db_base_and_session():
-    Base = automap_base()
-    engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + \
-                           "@" + config.db_host+ ":" + str(config.db_port) + "/" + config.db_name)
-    Base.prepare(engine, reflect=True)
-    session = Session(engine)
+    Base.prepare(Engine, reflect=True)
+    session = Session(Engine)
     return (Base, session)
