f4b66492292fbff3bb98d8c6bdecd9c977e7678c
Colson Driemel <colsond@uvic.ca>
2018-05-14 08:35:33 -0700

    create new engine for each session request, this should help avoid too many connection errors.

--- glintwebui/db_util.py
+++ glintwebui/db_util.py
@@ -4,12 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
-Base = automap_base()
-Engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + \
-                       "@" + config.db_host+ ":" + str(config.db_port) + "/" + config.db_name,
-                       pool_size=50, max_overflow=10)
 
 def get_db_base_and_session():
+	Base = automap_base()
+	Engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + \
+                           "@" + config.db_host+ ":" + str(config.db_port) + "/" + config.db_name,
+                           pool_size=50, max_overflow=10)
     Base.prepare(Engine, reflect=True)
     session = Session(Engine)
     return (Base, session)
